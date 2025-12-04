0271014bf6ff1d8c8fa0ef002a1673d64b675a29
Didymus343 <33725436+Didymus343@users.noreply.github.com>
2025-12-04 17:13:51 +0100

    Update Day01.cs

--- 2025/01/Day01.cs
+++ 2025/01/Day01.cs
@@ -13,9 +13,38 @@ namespace AoC2025
         {
             Loader.LoadToCharList(2025, 1, isTest, isDebug);
 
+             for (int i = 0; i < Loader.PuzzleInt.Count(); i++)
+        {
+            if (Loader.PuzzleInt[i] == 0)
+            {
+                caloriesPerElf.Add(totalCalories);
+                totalCalories = 0;
+
+            }
+            else
+            {
+                totalCalories += Loader.PuzzleInt[i];
+            }
+        }
+
             var list = new List<LinkedListNode>();
             list.Add(new LinkedListNode)
         }
+
+        public void Dial()
+{
+    var myList = new LinkedList<int>();            
+
+    for (int i = 0; i<= 99; i++)
+    {
+        var currentNode = new LinkedListNode<int>(i);
+        
+            myList.AddLast(currentNode);
+                      
+            
+    }
+
+}
     }
 
     static class CircularLinkedList 
